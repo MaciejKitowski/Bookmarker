d40cccf43f73cc887582e9e19b2defb956e57d7c
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-11-25 09:28:37 +0100

    Add Main class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.logging.Logger;
+
+public class Main {
+	private static final Logger log = Logger.getLogger(Main.class.getName());
+	
+	public static void main(String[] args) {
+		log.info("Run application");
+	}
+
+}
