7443950a46b663c6df0d89ca07be99937f6f1f5f
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-11-25 10:20:51 +0100

    Minor

--- src/mvc/dao/sqlite/SqliteDAOFactory.java
+++ src/mvc/dao/sqlite/SqliteDAOFactory.java
@@ -9,18 +9,23 @@ import mvc.dao.DAOFactory;
 
 public class SqliteDAOFactory extends DAOFactory {
 	private static final Logger log = Logger.getLogger(SqliteDAOFactory.class.getName());
+	
+	private static final String DRIVER = "org.sqlite.JDBC";
     private static final String URL = "jdbc:sqlite:sqlite.db";
     
     public static Connection getConnection() {
     	Connection connection = null;
     	
     	try {
+    		Class.forName(DRIVER);
     		connection = DriverManager.getConnection(URL);
     	}
     	catch(SQLException ex) {
     		log.warning(ex.getMessage());
     		log.warning(ex.getSQLState());
-    	}
+    	} catch (ClassNotFoundException ex) {
+			log.warning(ex.getMessage());
+		}
     	
     	return connection;
     }
