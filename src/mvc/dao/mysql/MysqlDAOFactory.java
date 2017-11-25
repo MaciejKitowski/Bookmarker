909836d3622b1440dc1c0a7baa610ca76beafd7c
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-11-25 10:58:07 +0100

    Set driver and url to mysql

--- src/mvc/dao/mysql/MysqlDAOFactory.java
+++ src/mvc/dao/mysql/MysqlDAOFactory.java
@@ -1,5 +1,8 @@
 package mvc.dao.mysql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import mvc.dao.sqlite.SqliteDAOFactory;
@@ -7,8 +10,26 @@ import mvc.dao.sqlite.SqliteDAOFactory;
 public class MysqlDAOFactory {
 	private static final Logger log = Logger.getLogger(SqliteDAOFactory.class.getName());
 	
-	private static final String DRIVER = "org.sqlite.JDBC";
-    private static final String URL = "jdbc:sqlite:sqlite.db";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost/bookmarker";
     private static final String USERNAME = "bookmarker";
     private static final String USERPASSWORD = "admin1";
+    
+    public static Connection getConnection() {
+    	log.info("Get connection");
+    	Connection connection = null;
+    	
+    	try {
+    		Class.forName(DRIVER);
+    		connection = DriverManager.getConnection(URL, USERNAME, USERPASSWORD);
+    	}
+    	catch(SQLException ex) {
+    		log.warning(ex.getMessage());
+    		log.warning(ex.getSQLState());
+    	} catch (ClassNotFoundException ex) {
+			log.warning(ex.getMessage());
+		}
+    	
+    	return connection;
+    }
 }
