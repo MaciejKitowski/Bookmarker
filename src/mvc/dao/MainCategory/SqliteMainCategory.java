02baca1fc8a0311cc6c7219cf46b03f3c7389472
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 13:59:54 +0100

    Implement interface

--- src/mvc/dao/MainCategory/SqliteMainCategory.java
+++ src/mvc/dao/MainCategory/SqliteMainCategory.java
@@ -1,8 +1,11 @@
 package mvc.dao.MainCategory;
 
+import java.util.List;
 import java.util.logging.Logger;
 
-public final class SqliteMainCategory {
+import mvc.model.MainCategory;
+
+public final class SqliteMainCategory implements IMainCategoryDAO {
 	private static final Logger log = Logger.getLogger(SqliteMainCategory.class.getName());
 	
 	private static final String CREATE_TABLE =
@@ -20,4 +23,45 @@ public final class SqliteMainCategory {
 	private static final String UPDATE = "UPDATE MainCategory SET name=? WHERE id = ?";
 	
 	private static final String DELETE = "DELETE FROM MainCategory WHERE id = ?";
+
+	@Override
+	public void createTable() {
+		log.info("Create new table");
+		
+	}
+
+	@Override
+	public int insert(MainCategory category) {
+		log.info(String.format("Insert category: ID=%d, name=%s"));
+
+		return 0;
+	}
+
+	@Override
+	public MainCategory get(int ID) {
+		log.info(String.format("Get category: ID=%d", ID));
+
+		return null;
+	}
+
+	@Override
+	public List<MainCategory> getAll() {
+		log.info("Get all categories");
+		
+		return null;
+	}
+
+	@Override
+	public boolean update(MainCategory category) {
+		log.info(String.format("Update category(ID=%d, name=%s): ID=%d, name=%s"));
+
+		return false;
+	}
+
+	@Override
+	public boolean delete(int ID) {
+		log.info(String.format("Delete category: ID=%d", ID));
+
+		return false;
+	}
 }
