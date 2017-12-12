b92a9155ffe7469d7a331215ef6a5ccde85c255f
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 13:50:55 +0100

    Added class

---
+++ src/mvc/dao/MainCategory/SqliteMainCategory.java
@@ -0,0 +1,7 @@
+package mvc.dao.MainCategory;
+
+import java.util.logging.Logger;
+
+public final class SqliteMainCategory {
+	private static final Logger log = Logger.getLogger(SqliteMainCategory.class.getName());
+}
