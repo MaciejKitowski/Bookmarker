42ff9ece149975dc73a563317df45f73c0c7cd02
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 15:08:11 +0100

    Added class

---
+++ src/mvc/dao/MainCategory/PostgresMainCategory.java
@@ -0,0 +1,7 @@
+package mvc.dao.MainCategory;
+
+import java.util.logging.Logger;
+
+public final class PostgresMainCategory {
+	private static final Logger log = Logger.getLogger(PostgresMainCategory.class.getName());
+}
