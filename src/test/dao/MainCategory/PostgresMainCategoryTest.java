e6eba6d4ee2a995b7391630fff2c46bd306b43f3
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 16:14:36 +0100

    Added create table test (not checked yet)

--- src/test/dao/MainCategory/PostgresMainCategoryTest.java
+++ src/test/dao/MainCategory/PostgresMainCategoryTest.java
@@ -1,5 +1,42 @@
 package test.dao.MainCategory;
 
-public class PostgresMainCategoryTest {
+import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import mvc.dao.PostgresFactory;
+import mvc.dao.MainCategory.IMainCategoryDAO;
+import mvc.dao.MainCategory.PostgresMainCategory;
+
+public class PostgresMainCategoryTest {
+	private List<String> getTableNames() throws Exception {
+		Connection connection = PostgresFactory.getConnection();
+		DatabaseMetaData meta = connection.getMetaData();
+		List<String> buffer = new ArrayList<>();
+		
+		//ResultSet rs = meta.getTables(null, null, "%", new String[] { "TABLE" });
+		ResultSet rs = meta.getTables(null, null, "%", null);
+		while(rs.next()) buffer.add(rs.getString(3));
+		
+		rs.close();
+		connection.close();
+		
+		return buffer;
+	}
+	
+	@Test
+	public void createTableTest() throws Exception {
+		IMainCategoryDAO catDAO = new PostgresMainCategory();
+		catDAO.createTable();
+		
+		List<String> tables = getTableNames();
+		
+		assertTrue(tables.contains("profile"));
+	}
 }
