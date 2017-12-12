9ebfa7e8694c5fead20cc6cd7447c54307d9c510
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 15:08:56 +0100

    Added SQL queries

--- src/mvc/dao/MainCategory/PostgresMainCategory.java
+++ src/mvc/dao/MainCategory/PostgresMainCategory.java
@@ -4,4 +4,20 @@ import java.util.logging.Logger;
 
 public final class PostgresMainCategory {
 	private static final Logger log = Logger.getLogger(PostgresMainCategory.class.getName());
+	
+	private static final String CREATE_TABLE = 
+		"CREATE TABLE profile( \n" +
+				"ID 	SERIAL 	PRIMARY KEY," +
+				"name 	VARCHAR(255) 	NOT NULL \n" +
+		")";
+	
+	private static final String INSERT = "INSERT INTO profile(name) VALUES(?)";
+	
+	private static final String GET = "SELECT ID, name FROM profile WHERE id = ?";
+	
+	private static final String GET_ALL = "SELECT ID, name FROM profile";
+	
+	private static final String UPDATE = "UPDATE profile SET name=? WHERE id = ?";
+	
+	private static final String DELETE = "DELETE FROM profile WHERE id = ?";
 }
