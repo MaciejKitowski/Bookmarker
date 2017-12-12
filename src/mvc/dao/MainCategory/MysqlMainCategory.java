a1a0616a499978018057aa33cc4f5bd3e1bfcbd2
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 14:42:40 +0100

    Implement interface

--- src/mvc/dao/MainCategory/MysqlMainCategory.java
+++ src/mvc/dao/MainCategory/MysqlMainCategory.java
@@ -1,10 +1,11 @@
 package mvc.dao.MainCategory;
 
+import java.util.List;
 import java.util.logging.Logger;
 
-import mvc.dao.mysql.MysqlProfile;
+import mvc.model.MainCategory;
 
-public final class MysqlMainCategory {
+public final class MysqlMainCategory implements IMainCategoryDAO {
 	private static final Logger log = Logger.getLogger(MysqlMainCategory.class.getName());
 	
 	private static final String CREATE_TABLE = 
@@ -23,4 +24,47 @@ public final class MysqlMainCategory {
 		private static final String UPDATE = "UPDATE MainCategory SET name=? WHERE id = ?";
 		
 		private static final String DELETE = "DELETE FROM MainCategory WHERE id = ?";
+
+		@Override
+		public void createTable() {
+			log.info("Create new table");
+			
+		}
+
+		@Override
+		public int insert(MainCategory category) {
+			log.info(String.format("Insert category: ID=%d, name=%s", category.getID(), category.getName()));
+
+			
+			
+			return 0;
+		}
+
+		@Override
+		public MainCategory get(int ID) {
+			log.info(String.format("Get category: ID=%d", ID));
+
+			
+			return null;
+		}
+
+		@Override
+		public List<MainCategory> getAll() {
+			log.info("Get all categories");
+
+			
+			return null;
+		}
+
+		@Override
+		public boolean update(MainCategory category) {
+			log.info(String.format("Update category: ID=%d, name=%s", category.getID(), category.getName()));
+			return false;
+		}
+
+		@Override
+		public boolean delete(int ID) {
+			log.info(String.format("Delete category: ID=%d", ID));
+			return false;
+		}
 }
