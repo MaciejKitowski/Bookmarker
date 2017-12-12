94b0e6e3be550b327a7384281016aef04a00f2a1
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 16:16:39 +0100

    Added multiple insert test (not checked yet)

--- src/test/dao/MainCategory/PostgresMainCategoryTest.java
+++ src/test/dao/MainCategory/PostgresMainCategoryTest.java
@@ -50,4 +50,19 @@ public class PostgresMainCategoryTest {
 		
 		assertNotEquals(result, PostgresMainCategory.INSERT_FAIL);
 	}
+	
+	@Test
+	public void insertMultipleTest() {
+		String pattern = "MultipleInsertTest_%d";
+		int insertCount = 10;
+		IMainCategoryDAO catDAO = new PostgresMainCategory();
+		
+		for(int i = 0; i < insertCount; ++i) {
+			MainCategory category = new MainCategory(String.format(pattern, i + 1));
+
+			int result = catDAO.insert(category);
+			
+			assertNotEquals(result, PostgresMainCategory.INSERT_FAIL);
+		}
+	}
 }
