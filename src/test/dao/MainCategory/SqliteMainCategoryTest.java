768d955f7ce57f40230e0205f6dcd00d3f9181d8
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 15:40:41 +0100

    Added create table test (not checked yet)

--- src/test/dao/MainCategory/SqliteMainCategoryTest.java
+++ src/test/dao/MainCategory/SqliteMainCategoryTest.java
@@ -1,5 +1,41 @@
 package test.dao.MainCategory;
 
-public class SqliteMainCategoryTest {
+import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import mvc.dao.SqliteFactory;
+import mvc.dao.MainCategory.IMainCategoryDAO;
+import mvc.dao.MainCategory.SqliteMainCategory;
+
+public class SqliteMainCategoryTest {
+	private List<String> getTableNames() throws Exception {
+		Connection connection = SqliteFactory.getConnection();
+		DatabaseMetaData meta = connection.getMetaData();
+		List<String> buffer = new ArrayList<>();
+		
+		ResultSet rs = meta.getTables(null, null, "%", null);
+		while(rs.next()) buffer.add(rs.getString(3));
+		
+		rs.close();
+		connection.close();
+		
+		return buffer;
+	}
+	
+	@Test
+	public void createTableTest() throws Exception {
+		IMainCategoryDAO catDAO = new SqliteMainCategory();
+		catDAO.createTable();
+		
+		List<String> tables = getTableNames();
+		
+		assertTrue(tables.contains("MainCategory"));
+	}
 }
