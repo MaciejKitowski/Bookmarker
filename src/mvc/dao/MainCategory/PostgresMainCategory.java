9d75d73a1529bdbd705992e664fde51d0f3bbde0
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-12 15:10:15 +0100

    Implement interface

--- src/mvc/dao/MainCategory/PostgresMainCategory.java
+++ src/mvc/dao/MainCategory/PostgresMainCategory.java
@@ -1,8 +1,11 @@
 package mvc.dao.MainCategory;
 
+import java.util.List;
 import java.util.logging.Logger;
 
-public final class PostgresMainCategory {
+import mvc.model.MainCategory;
+
+public final class PostgresMainCategory implements IMainCategoryDAO {
 	private static final Logger log = Logger.getLogger(PostgresMainCategory.class.getName());
 	
 	private static final String CREATE_TABLE = 
@@ -20,4 +23,40 @@ public final class PostgresMainCategory {
 	private static final String UPDATE = "UPDATE profile SET name=? WHERE id = ?";
 	
 	private static final String DELETE = "DELETE FROM profile WHERE id = ?";
+
+	@Override
+	public void createTable() {
+		log.info("Create new table");
+		
+	}
+
+	@Override
+	public int insert(MainCategory category) {
+		log.info(String.format("Insert category: ID=%d, name=%s", category.getID(), category.getName()));
+		return 0;
+	}
+
+	@Override
+	public MainCategory get(int ID) {
+		log.info(String.format("Get category: ID=%d", ID));
+		return null;
+	}
+
+	@Override
+	public List<MainCategory> getAll() {
+		log.info("Get all categories");
+		return null;
+	}
+
+	@Override
+	public boolean update(MainCategory category) {
+		log.info(String.format("Update category: ID=%d, name=%s", category.getID(), category.getName()));
+		return false;
+	}
+
+	@Override
+	public boolean delete(int ID) {
+		log.info(String.format("Delete category: ID=%d", ID));
+		return false;
+	}
 }
