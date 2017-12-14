d01b0113a9c6e2cceb216780e80b53b86aeb5cfa
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-14 14:21:37 +0100

    Get all test

--- src/test/dao/Category/SqliteCategoryTest.java
+++ src/test/dao/Category/SqliteCategoryTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,6 +33,22 @@ public class SqliteCategoryTest {
 		connection.close();
 		
 		return buffer;
+	}
+	
+	private int getProfileCount() throws Exception {
+		String sql = "SELECT COUNT(*) FROM Category";
+		int count = 0;
+		Connection connection = SqliteFactory.getConnection();
+		Statement statement = connection.createStatement();
+		
+		ResultSet rs = statement.executeQuery(sql);
+		if(rs != null && rs.next()) count = rs.getInt(1);
+		
+		rs.close();
+		statement.close();
+		connection.close();
+		
+		return count;
 	}
 	
 	@Test
@@ -81,5 +98,14 @@ public class SqliteCategoryTest {
 		Category result = dao.get(ID);
 		
 		assertTrue((result == null || result.getID() == ID));
+	}
+	
+	@Test
+	public void getAllTest() throws Exception {
+		ICategoryDAO dao = new SqliteCategory();
+		
+		List<Category> result = dao.getAll();
+		
+		assertTrue((result != null && result.size() == getProfileCount()));
 	}
 }
