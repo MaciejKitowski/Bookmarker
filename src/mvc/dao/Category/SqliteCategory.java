06a0e675f02655fc421ec79f272c57a544881fa5
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-14 13:48:54 +0100

    Get

--- src/mvc/dao/Category/SqliteCategory.java
+++ src/mvc/dao/Category/SqliteCategory.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import mvc.dao.SqliteFactory;
+import mvc.dao.MainCategory.IMainCategoryDAO;
+import mvc.dao.MainCategory.SqliteMainCategory;
 import mvc.model.Category;
 import mvc.model.MainCategory;
 
@@ -95,7 +97,36 @@ public final class SqliteCategory implements ICategoryDAO {
 	@Override
 	public Category get(int ID) {
 		log.info(String.format("Get category: ID=%d", ID));
-		return null;
+		
+		Category category = null;
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet result = null;
+		
+		try {
+			connection = SqliteFactory.getConnection();
+			statement = connection.prepareStatement(GET);
+			
+			statement.setInt(1, ID);
+			statement.execute();
+			
+			result = statement.getResultSet();
+			
+			if(result != null && result.next()) {
+				int foundID = result.getInt(1);
+				String foundName = result.getString(2);
+				int foundParentID = result.getInt(3);
+				
+				IMainCategoryDAO mainCategory = new SqliteMainCategory();
+								
+				category = new Category(foundID, foundName, mainCategory.get(foundParentID));
+			}
+		}
+		catch(Exception ex) {
+			log.warning(ex.getMessage());
+		}
+		
+		return category;
 	}
 
 	@Override
