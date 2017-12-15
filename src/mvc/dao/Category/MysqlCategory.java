bf9426c876d0bcf69154a1fd677b004280c8b06e
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 09:20:51 +0100

    Insert

--- src/mvc/dao/Category/MysqlCategory.java
+++ src/mvc/dao/Category/MysqlCategory.java
@@ -1,6 +1,8 @@
 package mvc.dao.Category;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.List;
 import java.util.logging.Logger;
@@ -63,7 +65,33 @@ public final class MysqlCategory implements ICategoryDAO {
 	public int insert(Category category) {
 		log.info(String.format("Insert category: ID=%d, name=%s, parent: ID=%d, name=%s", category.getID(), category.getName(), category.getParent().getID(), category.getParent().getName()));
 
-		return 0;
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet result = null;
+		int resultBuffer = 0;
+		
+		try {
+			connection = MysqlFactory.getConnection();
+			statement = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+			
+			statement.setString(1, category.getName());
+			statement.setInt(2, category.getParent().getID());
+			statement.execute();
+			
+			result = statement.getGeneratedKeys();
+			if(result != null && result.next()) resultBuffer = result.getInt(1);
+			else resultBuffer = INSERT_FAIL;
+			
+			result.close();
+			statement.close();
+			connection.close();
+		}
+		catch(Exception ex) {
+			log.warning(ex.getMessage());
+			resultBuffer = INSERT_FAIL;
+		}
+
+		return resultBuffer;
 	}
 
 	@Override
