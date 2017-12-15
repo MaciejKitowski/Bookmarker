e851ce9c93c4f53b927e72eb22c40ae95f59600b
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 12:47:09 +0100

    Add SQL queries

--- src/mvc/dao/Url/MysqlUrl.java
+++ src/mvc/dao/Url/MysqlUrl.java
@@ -1,5 +1,34 @@
 package mvc.dao.Url;
 
-public final class MysqlUrl {
+import java.util.logging.Logger;
 
+public final class MysqlUrl {
+	private static final Logger log = Logger.getLogger(MysqlUrl.class.getName());
+	
+	public static int INSERT_FAIL = -1;
+		
+	private static final String CREATE_TABLE =
+		"CREATE TABLE Url( \n" +
+				"ID 	int 	NOT NULL	AUTO_INCREMENT, \n" +
+				"cat_ID 	int, \n" +
+				"title 	VARCHAR(255) 	NOT NULL, \n" +
+				"url 	VARCHAR(255) 	NOT NULL, \n" +
+				"description 	VARCHAR(255), \n" +
+				"PRIMARY KEY(ID), \n" +
+				"FOREIGN KEY(cat_ID) REFERENCES Category(ID) \n" +
+					"ON UPDATE CASCADE \n" +
+					"ON DELETE SET NULL \n" +
+		")";
+	
+	private static final String INSERT = "INSERT INTO Url(title, url, description, cat_ID) VALUES(?, ?, ?, ?)";
+	
+	private static final String GET = "SELECT ID, title, url, description, cat_ID FROM Url WHERE ID = ?";
+	
+	private static final String GET_CATEGORY = "SELECT ID, title, url, description, cat_ID FROM Url WHERE cat_ID = ?";
+	
+	private static final String GET_ALL = "SELECT ID, title, url, description, cat_ID FROM Url";
+	
+	private static final String UPDATE = "UPDATE Url SET title=?, url=?, description=?, cat_ID=? WHERE ID = ?";
+	
+	private static final String DELETE = "DELETE FROM Url WHERE id = ?";
 }
