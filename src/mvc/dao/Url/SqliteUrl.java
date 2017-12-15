76d75cc64037776225670430306b68e93e06dc49
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 12:05:12 +0100

    Create table

--- src/mvc/dao/Url/SqliteUrl.java
+++ src/mvc/dao/Url/SqliteUrl.java
@@ -1,8 +1,11 @@
 package mvc.dao.Url;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.List;
 import java.util.logging.Logger;
 
+import mvc.dao.SqliteFactory;
 import mvc.dao.Category.SqliteCategory;
 import mvc.model.Category;
 import mvc.model.Url;
@@ -38,6 +41,21 @@ public final class SqliteUrl implements IUrlDAO {
 	public void createTable() {
 		log.info("Create new table");
 		
+		Connection connection = null;
+		Statement statement = null;
+		
+		try {
+			connection = SqliteFactory.getConnection();
+			statement = connection.createStatement();
+			
+			statement.execute(CREATE_TABLE);
+			
+			statement.close();
+			connection.close();
+		}
+		catch(Exception ex) {
+			log.warning(ex.getMessage());
+		}
 	}
 
 	@Override
