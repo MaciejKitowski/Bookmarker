afc3cc2865dcc8613b4932ca902d4f1c40d91bf5
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 09:19:35 +0100

    Implement interface

--- src/mvc/dao/Category/MysqlCategory.java
+++ src/mvc/dao/Category/MysqlCategory.java
@@ -1,8 +1,12 @@
 package mvc.dao.Category;
 
+import java.util.List;
 import java.util.logging.Logger;
 
-public final class MysqlCategory {
+import mvc.model.Category;
+import mvc.model.MainCategory;
+
+public final class MysqlCategory implements ICategoryDAO {
 	private static final Logger log = Logger.getLogger(MysqlCategory.class.getName());
 	
 	public static int INSERT_FAIL = -1;
@@ -29,4 +33,52 @@ public final class MysqlCategory {
 	private static final String UPDATE = "UPDATE Category SET name=?, parent_ID=? WHERE id = ?";
 	
 	private static final String DELETE = "DELETE FROM Category WHERE id = ?";
+
+	@Override
+	public void createTable() {
+		log.info("Create new table");
+		
+	}
+
+	@Override
+	public int insert(Category category) {
+		log.info(String.format("Insert category: ID=%d, name=%s, parent: ID=%d, name=%s", category.getID(), category.getName(), category.getParent().getID(), category.getParent().getName()));
+
+		return 0;
+	}
+
+	@Override
+	public Category get(int ID) {
+		log.info(String.format("Get category: ID=%d", ID));
+		
+		return null;
+	}
+
+	@Override
+	public List<Category> getAllParent(MainCategory mainCategory) {
+		log.info(String.format("Get all categories with parent: ID=%d, name=%s", mainCategory.getID(), mainCategory.getName()));
+		
+		return null;
+	}
+
+	@Override
+	public List<Category> getAll() {
+		log.info("Get all categories");
+		
+		return null;
+	}
+
+	@Override
+	public boolean update(Category category) {
+		log.info(String.format("Update category: ID=%d, name=%s", category.getID(), category.getName()));
+		
+		return false;
+	}
+
+	@Override
+	public boolean delete(int ID) {
+		log.info(String.format("Delete category: ID=%d", ID));
+		
+		return false;
+	}
 }
