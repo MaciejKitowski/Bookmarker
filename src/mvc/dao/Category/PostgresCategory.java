30a0acb85603aa5e4c4e675b0e5d0f1f5c3a5ea8
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 09:37:28 +0100

    Added class

---
+++ src/mvc/dao/Category/PostgresCategory.java
@@ -0,0 +1,7 @@
+package mvc.dao.Category;
+
+import java.util.logging.Logger;
+
+public final class PostgresCategory {
+	private static final Logger log = Logger.getLogger(PostgresCategory.class.getName());
+}
