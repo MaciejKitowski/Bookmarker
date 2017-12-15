4b0519058c775fc60bb20ef07f0ba7224646413d
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 12:01:17 +0100

    Add SQL queries

--- src/mvc/dao/Url/SqliteUrl.java
+++ src/mvc/dao/Url/SqliteUrl.java
@@ -8,4 +8,26 @@ public final class SqliteUrl {
 	private static final Logger log = Logger.getLogger(SqliteUrl.class.getName());
 	
 	public static int INSERT_FAIL = -1;
+	
+	private static final String CREATE_TABLE =
+		"CREATE TABLE Url( \n" +
+				"ID 	INTEGER 	PRIMARY KEY AUTOINCREMENT, \n" +
+				"cat_ID 	INTEGER, \n" +
+				"title 	VARCHAR(255) 	NOT NULL, \n" +
+				"url 	VARCHAR(255) 	NOT NULL, \n" +
+				"description 	VARCHAR(255), \n" +
+				"FOREIGN KEY(cat_ID) REFERENCES Category(ID) \n" +
+					"ON UPDATE CASCADE \n" +
+					"ON DELETE SET NULL \n" +
+		")";
+	
+	private static final String INSERT = "INSERT INTO Url(title, url, description, cat_ID) VALUES(?, ?, ?, ?)";
+	
+	private static final String GET = "SELECT ID, title, url, description, cat_ID FROM Url WHERE ID = ?";
+	
+	private static final String GET_CATEGORY = "SELECT ID, title, url, description, cat_ID FROM Url WHERE cat_ID = ?";
+	
+	private static final String UPDATE = "UPDATE Url SET title=?, url=?, title=?, description=?, cat_ID=? WHERE ID = ?";
+	
+	private static final String DELETE = "DELETE FROM Url WHERE id = ?";
 }
