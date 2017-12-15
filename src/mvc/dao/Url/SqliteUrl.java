bce03db41e5ff72c23d87bbe1e0f622c8614e15f
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 12:04:38 +0100

    Implement interface

--- src/mvc/dao/Url/SqliteUrl.java
+++ src/mvc/dao/Url/SqliteUrl.java
@@ -1,10 +1,13 @@
 package mvc.dao.Url;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import mvc.dao.Category.SqliteCategory;
+import mvc.model.Category;
+import mvc.model.Url;
 
-public final class SqliteUrl {
+public final class SqliteUrl implements IUrlDAO {
 	private static final Logger log = Logger.getLogger(SqliteUrl.class.getName());
 	
 	public static int INSERT_FAIL = -1;
@@ -30,4 +33,52 @@ public final class SqliteUrl {
 	private static final String UPDATE = "UPDATE Url SET title=?, url=?, title=?, description=?, cat_ID=? WHERE ID = ?";
 	
 	private static final String DELETE = "DELETE FROM Url WHERE id = ?";
+
+	@Override
+	public void createTable() {
+		log.info("Create new table");
+		
+	}
+
+	@Override
+	public int insert(Url url) {
+		log.info(String.format("Insert url: ID=%d, title=%s, url=%s", url.getID(), url.getTitle(), url.getTitle()));
+		
+		return 0;
+	}
+
+	@Override
+	public Url get(int ID) {
+		log.info(String.format("Get url: ID=%d", ID));
+		
+		return null;
+	}
+
+	@Override
+	public List<Url> getAllParent(Category category) {
+		log.info(String.format("Get all urls with category: ID=%d, name=%s", category.getID(), category.getName()));
+		
+		return null;
+	}
+
+	@Override
+	public List<Url> getAll() {
+		log.info("Get all urls");
+		
+		return null;
+	}
+
+	@Override
+	public boolean update(Url url) {
+		log.info(String.format("Update url: ID=%d, url=%s", url.getID(), url.getUrl()));
+		
+		return false;
+	}
+
+	@Override
+	public boolean delete(int ID) {
+		log.info(String.format("Delete url: ID=%d", ID));
+		
+		return false;
+	}
 }
