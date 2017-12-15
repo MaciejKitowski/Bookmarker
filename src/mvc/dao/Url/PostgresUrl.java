9c0a36280f28bad5364e20d1fa25fea718e23930
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 13:04:33 +0100

    Insert

--- src/mvc/dao/Url/PostgresUrl.java
+++ src/mvc/dao/Url/PostgresUrl.java
@@ -1,11 +1,14 @@
 package mvc.dao.Url;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.logging.Logger;
 
 import mvc.dao.PostgresFactory;
 import mvc.dao.SqliteFactory;
+import mvc.model.Url;
 
 public final class PostgresUrl implements IUrlDAO {
 	private static final Logger log = Logger.getLogger(PostgresUrl.class.getName());
@@ -24,36 +27,71 @@ public final class PostgresUrl implements IUrlDAO {
 					"ON DELETE SET NULL \n" +
 		")";
 		
-		private static final String INSERT = "INSERT INTO Url(title, url, description, cat_ID) VALUES(?, ?, ?, ?)";
+	private static final String INSERT = "INSERT INTO Url(title, url, description, cat_ID) VALUES(?, ?, ?, ?)";
+	
+	private static final String GET = "SELECT ID, title, url, description, cat_ID FROM Url WHERE ID = ?";
+	
+	private static final String GET_CATEGORY = "SELECT ID, title, url, description, cat_ID FROM Url WHERE cat_ID = ?";
+	
+	private static final String GET_ALL = "SELECT ID, title, url, description, cat_ID FROM Url";
+	
+	private static final String UPDATE = "UPDATE Url SET title=?, url=?, description=?, cat_ID=? WHERE ID = ?";
+	
+	private static final String DELETE = "DELETE FROM Url WHERE id = ?";
 		
-		private static final String GET = "SELECT ID, title, url, description, cat_ID FROM Url WHERE ID = ?";
+	@Override
+	public void createTable() {
+		log.info("Create new table");
 		
-		private static final String GET_CATEGORY = "SELECT ID, title, url, description, cat_ID FROM Url WHERE cat_ID = ?";
+		Connection connection = null;
+		Statement statement = null;
 		
-		private static final String GET_ALL = "SELECT ID, title, url, description, cat_ID FROM Url";
-		
-		private static final String UPDATE = "UPDATE Url SET title=?, url=?, description=?, cat_ID=? WHERE ID = ?";
-		
-		private static final String DELETE = "DELETE FROM Url WHERE id = ?";
-		
-		@Override
-		public void createTable() {
-			log.info("Create new table");
+		try {
+			connection = PostgresFactory.getConnection();
+			statement = connection.createStatement();
 			
-			Connection connection = null;
-			Statement statement = null;
+			statement.execute(CREATE_TABLE);
 			
-			try {
-				connection = PostgresFactory.getConnection();
-				statement = connection.createStatement();
-				
-				statement.execute(CREATE_TABLE);
-				
-				statement.close();
-				connection.close();
-			}
-			catch(Exception ex) {
-				log.warning(ex.getMessage());
-			}
+			statement.close();
+			connection.close();
 		}
+		catch(Exception ex) {
+			log.warning(ex.getMessage());
+		}
+	}
+	
+	@Override
+	public int insert(Url url) {
+		log.info(String.format("Insert url: ID=%d, title=%s, url=%s", url.getID(), url.getTitle(), url.getTitle()));
+		
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet result = null;
+		int resultBuffer = 0;
+			
+		try {
+			connection = PostgresFactory.getConnection();
+			statement = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+			
+			statement.setString(1, url.getTitle());
+			statement.setString(2, url.getUrl());
+			statement.setString(3, url.getDescription());
+			statement.setInt(4, url.getCategory().getID());
+			statement.execute();
+			
+			result = statement.getGeneratedKeys();
+			if(result != null && result.next()) resultBuffer = result.getInt(1);
+			else resultBuffer = INSERT_FAIL;
+			
+			result.close();
+			statement.close();
+			connection.close();
+		}
+		catch(Exception ex) {
+			log.warning(ex.getMessage());
+			resultBuffer = INSERT_FAIL;
+		}
+
+		return resultBuffer;
+	}
 }
