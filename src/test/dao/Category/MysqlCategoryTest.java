da32efc0fd6f4a104cd13408a9a19ba2ca777428
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 09:32:03 +0100

    Get all test

--- src/test/dao/Category/MysqlCategoryTest.java
+++ src/test/dao/Category/MysqlCategoryTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +35,22 @@ public class MysqlCategoryTest {
 		connection.close();
 		
 		return buffer;
+	}
+	
+	private int getProfileCount() throws Exception {
+		String sql = "SELECT COUNT(*) FROM Category";
+		int count = 0;
+		Connection connection = SqliteFactory.getConnection();
+		Statement statement = connection.createStatement();
+		
+		ResultSet rs = statement.executeQuery(sql);
+		if(rs != null && rs.next()) count = rs.getInt(1);
+		
+		rs.close();
+		statement.close();
+		connection.close();
+		
+		return count;
 	}
 	
 	@Test
@@ -83,5 +100,14 @@ public class MysqlCategoryTest {
 		Category result = dao.get(ID);
 		
 		assertTrue((result == null || result.getID() == ID));
+	}
+	
+	@Test
+	public void getAllTest() throws Exception {
+		ICategoryDAO dao = new MysqlCategory();
+		
+		List<Category> result = dao.getAll();
+		
+		assertTrue((result != null && result.size() == getProfileCount()));
 	}
 }
