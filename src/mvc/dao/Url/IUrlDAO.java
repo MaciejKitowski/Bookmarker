1e55507816f1f95c5160e5228fd51f10517b980e
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 11:53:31 +0100

    Added Url DAO interface

---
+++ src/mvc/dao/Url/IUrlDAO.java
@@ -0,0 +1,18 @@
+package mvc.dao.Url;
+
+import java.util.List;
+
+import mvc.model.Category;
+import mvc.model.Url;
+
+public interface IUrlDAO {
+	public void createTable();
+	public int insert(Url url);
+	
+	public Url get(int ID);
+	public List<Url> getAllParent(Category category);
+	public List<Url> getAll();
+	
+	public boolean update(Url url);
+	public boolean delete(int ID);
+}
