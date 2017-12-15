3189e3a2b723f64b0b54e548ded78e2550a6428e
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 11:56:44 +0100

    Add class

---
+++ src/mvc/dao/Url/SqliteUrl.java
@@ -0,0 +1,11 @@
+package mvc.dao.Url;
+
+import java.util.logging.Logger;
+
+import mvc.dao.Category.SqliteCategory;
+
+public final class SqliteUrl {
+	private static final Logger log = Logger.getLogger(SqliteUrl.class.getName());
+	
+	public static int INSERT_FAIL = -1;
+}
