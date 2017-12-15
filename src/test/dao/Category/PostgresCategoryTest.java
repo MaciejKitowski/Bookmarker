3ab60e3e3cef93eff8e90e5b35acf0413b8b5dbf
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-15 09:51:50 +0100

    Create table test

---
+++ src/test/dao/Category/PostgresCategoryTest.java
@@ -0,0 +1,60 @@
+package test.dao.Category;
+
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import mvc.dao.MysqlFactory;
+import mvc.dao.PostgresFactory;
+import mvc.dao.Category.ICategoryDAO;
+import mvc.dao.Category.MysqlCategory;
+import mvc.dao.Category.PostgresCategory;
+
+public class PostgresCategoryTest {
+	private List<String> getTableNames() throws Exception {
+		Connection connection = PostgresFactory.getConnection();
+		DatabaseMetaData meta = connection.getMetaData();
+		List<String> buffer = new ArrayList<>();
+		
+		ResultSet rs = meta.getTables(null, null, "%", null);
+		while(rs.next()) buffer.add(rs.getString(3));
+		
+		rs.close();
+		connection.close();
+		
+		return buffer;
+	}
+	
+	private int getProfileCount() throws Exception {
+		String sql = "SELECT COUNT(*) FROM Category";
+		int count = 0;
+		Connection connection = PostgresFactory.getConnection();
+		Statement statement = connection.createStatement();
+		
+		ResultSet rs = statement.executeQuery(sql);
+		if(rs != null && rs.next()) count = rs.getInt(1);
+		
+		rs.close();
+		statement.close();
+		connection.close();
+		
+		return count;
+	}
+	
+	@Test
+	public void createTableTest() throws Exception {
+		ICategoryDAO dao = new PostgresCategory();
+		dao.createTable();
+		
+		List<String> tables = getTableNames();
+		
+		assertTrue(tables.contains("category"));
+	}
+}
