2330b9562a715dae7abec222350728dabcf7e2d9
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-16 13:39:00 +0100

    Load JSON file

--- src/mvc/dao/MainCategory/MainCategoryDAO.java
+++ src/mvc/dao/MainCategory/MainCategoryDAO.java
@@ -1,15 +1,25 @@
 package mvc.dao.MainCategory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
+
+import mvc.dao.DAOFactory;
 
 public final class MainCategoryDAO {
 	private static final Logger log = Logger.getLogger(MainCategoryDAO.class.getName());
-	private static final String queryFilename = "MainCategory.dao";
+	private static final String queryFilename = "MainCategory.json";
 	
 	public static int INSERT_FAIL = -1;
 	
+	private DAOFactory database = null;
 	private String CREATE_TABLE = null;
 	private String INSERT = null;
 	private String GET = null;
@@ -17,11 +27,28 @@ public final class MainCategoryDAO {
 	private String UPDATE = null;
 	private String DELETE = null;
 	
-	private void loadQueriesFromFile(String dbName) {
+	public MainCategoryDAO(int databaseType) {
+		database = DAOFactory.get(databaseType);
+		
+		loadQueriesFromFile();
+	}
+	
+	private void loadQueriesFromFile() {
 		log.info("Load SQL queries from: " + queryFilename);
 		
-		JSONObject parent = new JSONObject(queryFilename);
+		try {
+			FileInputStream jsonFile = new FileInputStream(new File(queryFilename));
+			String rawJson = IOUtils.toString(jsonFile);
+			
+			System.out.println(rawJson);
+		}
+		catch(Exception ex) {
+			log.warning(ex.getMessage());
+		}
 		
-		log.info(parent.toString());
+		/*JSONObject parent = new JSONObject();
+		
+		System.out.println("XX");
+		System.out.println(parent.toString());*/
 	}
 }
