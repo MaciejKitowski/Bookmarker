80a28d7f7d988425fe0b3440d28c2615155f45ec
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-18 10:40:47 +0100

    Count values in table

--- src/test/dao/DAOutils.java
+++ src/test/dao/DAOutils.java
@@ -3,10 +3,12 @@ package test.dao;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import mvc.dao.DAOFactory;
+import mvc.dao.SqliteFactory;
 
 public class DAOutils {
 	private DAOutils() {}
@@ -23,5 +25,21 @@ public class DAOutils {
 		connection.close();
 		
 		return buffer;
+	}
+	
+	public static int count(String tableName, int database) throws Exception {
+		String sql = "SELECT COUNT(*) FROM " + tableName;
+		int result = 0;
+		Connection connection = DAOFactory.get(database).createConnection();
+		Statement statement = connection.createStatement();
+		
+		ResultSet rs = statement.executeQuery(sql);
+		if(rs != null && rs.next()) result = rs.getInt(1);
+		
+		rs.close();
+		statement.close();
+		connection.close();
+		
+		return result;
 	}
 }
