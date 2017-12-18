5f20fa438c06dacc7aaf8b0928b96be9c5158cb2
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-18 10:23:01 +0100

    Insert test

--- src/test/dao/MainCategory/MainCategoryDAOTest.java
+++ src/test/dao/MainCategory/MainCategoryDAOTest.java
@@ -7,11 +7,13 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.internal.runners.statements.Fail;
 import org.junit.runners.MethodSorters;
 
 import mvc.dao.DAOFactory;
+import mvc.dao.MainCategory.IMainCategoryDAO;
 import mvc.dao.MainCategory.MainCategoryDAO;
+import mvc.dao.MainCategory.SqliteMainCategory;
+import mvc.model.MainCategory;
 import test.dao.DAOutils;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -27,7 +29,6 @@ public class MainCategoryDAOTest {
 	public void createTableTest() {
 		try {
 			dao.createTable();
-			
 			List<String> tableList = DAOutils.getTableNames(DAOFactory.SQLITE);
 			
 			assertTrue(tableList.contains("MainCategory"));
@@ -35,5 +36,14 @@ public class MainCategoryDAOTest {
 		catch(Exception ex) {
 			fail(ex.getMessage());
 		}
+	}
+	
+	@Test
+	public void insertTest() {
+		MainCategory category = new MainCategory("SingleInsertTest");
+		
+		int result = dao.insert(category);
+		
+		assertNotEquals(MainCategoryDAO.INSERT_FAIL, result);
 	}
 }
