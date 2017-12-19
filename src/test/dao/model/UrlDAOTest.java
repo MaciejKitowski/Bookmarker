705c3583cec1d3069e9210615887975e26550265
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-19 18:01:36 +0100

    Load queries test

--- src/test/dao/model/UrlDAOTest.java
+++ src/test/dao/model/UrlDAOTest.java
@@ -1,9 +1,16 @@
 package test.dao.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -11,6 +18,7 @@ import org.junit.runners.Parameterized.Parameters;
 import mvc.dao.DAOFactory;
 import mvc.dao.Category.CategoryDAO;
 import mvc.dao.Url.UrlDAO;
+import test.dao.DAOutils;
 
 @RunWith(Parameterized.class)
 public class UrlDAOTest {
@@ -31,5 +39,30 @@ public class UrlDAOTest {
 	@Before
 	public void initialize() {
 		dao = new UrlDAO(databaseType);
+	}
+	
+	@Test
+	public void loadQueriesTest() {
+		String[] fieldNames = {"CREATE_TABLE", "INSERT", "GET", "GET_CATEGORY", "GET_ALL", "UPDATE", "DELETE"};
+		
+		try {
+			JSONObject json = DAOutils.getJsonQuery("Url.json", databaseType);
+			Class<?> cls = dao.getClass();
+			
+			for(String name : fieldNames) {
+				String sql = null;
+				Field field = cls.getDeclaredField(name);
+				field.setAccessible(true);
+				
+				if(name == "CREATE_TABLE") sql = DAOutils.getCreateTable(json.getJSONArray(name));
+				else sql = json.getString(name);
+				
+				assertNotNull(field.get(dao));
+				assertEquals(field.get(dao), sql);
+			}
+		}
+		catch(Exception ex) {
+			fail(ex.getMessage());
+		}
 	}
 }
