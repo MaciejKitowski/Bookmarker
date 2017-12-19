473afb10e8c490d981b0d221c07d9dd8faa5f253
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-19 14:36:28 +0100

    Added class

---
+++ src/mvc/dao/Url/UrlDAO.java
@@ -0,0 +1,9 @@
+package mvc.dao.Url;
+
+import java.util.logging.Logger;
+
+public final class UrlDAO {
+	private static final Logger log = Logger.getLogger(UrlDAO.class.getName());
+	
+	public static int INSERT_FAIL = -1;
+}
