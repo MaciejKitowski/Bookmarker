f94cdde18d546cd6d645d74ec3c9f398ce5c7ef8
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-19 17:50:08 +0100

    Load SQL queries from file

--- src/mvc/dao/Url/UrlDAO.java
+++ src/mvc/dao/Url/UrlDAO.java
@@ -1,7 +1,13 @@
 package mvc.dao.Url;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 import java.util.logging.Logger;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import mvc.dao.DAOFactory;
 import mvc.model.Category;
@@ -9,6 +15,7 @@ import mvc.model.Url;
 
 public final class UrlDAO implements IUrlDAO {
 	private static final Logger log = Logger.getLogger(UrlDAO.class.getName());
+	private static final String queryFilename = "Url.json";
 	
 	public static int INSERT_FAIL = -1;
 	
@@ -20,6 +27,12 @@ public final class UrlDAO implements IUrlDAO {
 	private String GET_ALL = null;
 	private String UPDATE = null;
 	private String DELETE = null;
+	
+	public UrlDAO(int databaseType) {
+		database = DAOFactory.get(databaseType);
+		
+		loadQueriesFromFile();
+	}
 
 	@Override
 	public void createTable() {
@@ -63,5 +76,35 @@ public final class UrlDAO implements IUrlDAO {
 	public boolean delete(int ID) {
 		log.info(String.format("Delete url: ID=%d", ID));
 		return false;
+	}
+	
+	private void loadQueriesFromFile() {
+		log.info("Load SQL queries from: " + queryFilename);
+		
+		FileInputStream jsonFile = null;
+		
+		try {
+			jsonFile = new FileInputStream(new File(queryFilename));
+			String rawJson = IOUtils.toString(jsonFile);
+			JSONObject obj = new JSONObject(rawJson).getJSONObject(database.getName());
+			
+			CREATE_TABLE = getCreateTable(obj.getJSONArray("CREATE_TABLE"));
+			INSERT = obj.getString("INSERT");
+			GET = obj.getString("GET");
+			GET_ALL = obj.getString("GET_ALL");
+			GET_CATEGORY = obj.getString("GET_CATEGORY");
+			UPDATE = obj.getString("UPDATE");
+			DELETE = obj.getString("DELETE");
+			
+			jsonFile.close();
+		}
+		catch(Exception ex) {
+			log.warning(ex.getMessage());
+		}
+	}
+	
+	private String getCreateTable(JSONArray array) {
+		String[] raw = array.toList().toArray(new String[array.length()]);
+		return String.join("\n", raw);
 	}
 }
