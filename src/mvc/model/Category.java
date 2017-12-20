770288c4f4b61827bc5d9bb91a67d340e4dcf98e
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-20 10:14:49 +0100

    Better logging in Category model

--- src/mvc/model/Category.java
+++ src/mvc/model/Category.java
@@ -10,25 +10,25 @@ public final class Category {
 	private MainCategory parent = null;
 	
 	public Category(int ID, String name, MainCategory parent) {
-		log.info(String.format("Create new category: ID=%d, name=%s parent: ID=%d, name=%s", ID, name, parent.getID(), parent.getName()));
+		log.info(String.format("New Category \n\t ID=%d \n\t name=%s \n\t parent: \n\t\t ID=%d \n\t\t name=%s", ID, name, parent.getID(), parent.getName()));
 		this.ID = ID;
 		this.name = name;
 		this.parent = parent;
 	}
 	
 	public Category(int ID, String name) {
-		log.info(String.format("Create new category: ID=%d, name=%s", ID, name));
+		log.info(String.format("New Category \n\t ID=%d \n\t name=%s \n\t parent: \n\t\t ID=%d \n\t\t name=%s", ID, name, null, null));
 		this.ID = ID;
 		this.name = name;
 	}
 	
 	public Category(String name) {
-		log.info("Create category without ID: " + name);
+		log.info(String.format("New Category \n\t ID=%d \n\t name=%s \n\t parent: \n\t\t ID=%d \n\t\t name=%s", null, name, null, null));
 		this.name = name;
 	}
 	
 	public Category() {
-		log.info("Create empty category");
+		log.info(String.format("New Category \n\t ID=%d \n\t name=%s \n\t parent: \n\t\t ID=%d \n\t\t name=%s", null, null, null, null));
 		ID = 0;
 		name = "EMPTY";
 		parent = new MainCategory();
