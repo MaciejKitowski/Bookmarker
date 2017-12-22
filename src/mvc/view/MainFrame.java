0b9b4c305a2ff9380d96f07d99f20195aab7a92c
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-22 09:59:01 +0100

    Minor changes

--- src/mvc/view/MainFrame.java
+++ src/mvc/view/MainFrame.java
@@ -6,15 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class MainFrame extends JFrame {
+	private static final long serialVersionUID = 4785143357028575468L;
 	private static final Logger log = LoggerFactory.getLogger(MainFrame.class);
-	private String frameName = "Bookmarker";
+	private String windowTitle = "Bookmarker";
 	private int defaultWidth = 800;
 	private int defaultHeight = 600;
 	
 	public MainFrame() {
-		log.info("Initialize Main Frame");
+		log.info("Initialize Main Frame with title: {} and size: {}x{}", windowTitle, defaultWidth, defaultHeight);
 		
-		setTitle(frameName);
+		setTitle(windowTitle);
 		setSize(defaultWidth, defaultHeight);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
