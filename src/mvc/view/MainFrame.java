38957b571de5130a6616b7464aaec36c8ff20b40
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-22 09:57:01 +0100

    Display frame

--- src/mvc/view/MainFrame.java
+++ src/mvc/view/MainFrame.java
@@ -1,5 +1,21 @@
 package mvc.view;
 
-public final class MainFrame {
+import javax.swing.JFrame;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class MainFrame extends JFrame {
+	private static final Logger log = LoggerFactory.getLogger(MainFrame.class);
+	private String frameName = "Bookmarker";
+	private int defaultWidth = 800;
+	private int defaultHeight = 600;
+	
+	public MainFrame() {
+		log.info("Initialize Main Frame");
+		
+		setTitle(frameName);
+		setSize(defaultWidth, defaultHeight);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
