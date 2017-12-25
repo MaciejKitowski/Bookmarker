7a671fe14f4ea288669b86325e6a41b46078edca
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-25 15:38:53 +0100

    Set columns prefered width

--- src/mvc/view/UrlView.java
+++ src/mvc/view/UrlView.java
@@ -4,11 +4,17 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.util.Enumeration;
+import java.util.Set;
+
+import javax.lang.model.element.VariableElement;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +37,7 @@ public final class UrlView extends JPanel {
 				
 		initializeTable();
 		setTableStyle();
+		setTableColumnsSize();
 		add(tableScroll, BorderLayout.CENTER);
 	}
 	
@@ -50,5 +57,21 @@ public final class UrlView extends JPanel {
 		table.setAutoCreateRowSorter(true);
 		
 		tableScroll.getViewport().setBackground(Color.GREEN);
+	}
+	
+	private void setTableColumnsSize() {
+		log.debug("Set table columns size");
+		
+		setColumnWidth(columnNames[0], 30, 60);
+		setColumnWidth(columnNames[1], 100, 300);
+		setColumnWidth(columnNames[2], 100, 300);
+		setColumnWidth(columnNames[3], 10, 200);
+	}
+	
+	private void setColumnWidth(String name, int min, int prefered) {
+		log.debug("Set width for column: {} (min={}, prefered={})", name, min, prefered);
+		
+		table.getColumn(name).setMinWidth(min);
+		table.getColumn(name).setPreferredWidth(prefered);
 	}
 }
