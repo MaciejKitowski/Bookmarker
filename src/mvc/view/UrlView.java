27237bb76ac6fb3bb92f1240ac4e186db0f5ee98
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-25 14:54:57 +0100

    Initialize table

--- src/mvc/view/UrlView.java
+++ src/mvc/view/UrlView.java
@@ -2,9 +2,16 @@ package mvc.view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.tree.DefaultTreeCellRenderer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +21,35 @@ public final class UrlView extends JPanel {
 	private static final Logger log = LoggerFactory.getLogger(UrlView.class);
 	private static final String[] columnNames = {"ID", "Title", "Url", "Description"};
 	
+	private DefaultTableModel tableModel = null;
+	private JTable table = null;
+	private JScrollPane tableScroll = null;
+	
 	public UrlView(int width, int height) {
 		log.info("Initialize Url view");
 		
 		setBackground(Color.GREEN);
 		setPreferredSize(new Dimension(width, height));
 		setBorder(new TitledBorder("Urls"));
+				
+		initializeTable();
+		setTableStyle();
+		add(tableScroll);
+	}
+	
+	private void initializeTable() {
+		log.debug("Initialize table");
+		
+		tableModel = new DefaultTableModel(columnNames, 0);
+		table = new JTable(tableModel);
+		tableScroll = new JScrollPane(table);
+	}
+	
+	private void setTableStyle() {
+		log.debug("Set table style");
+		
+		table.setFont(new Font("Arial", Font.PLAIN, 26));
+		table.setRowHeight(table.getRowHeight() + 16);
+		table.setAutoCreateRowSorter(true);
 	}
 }
