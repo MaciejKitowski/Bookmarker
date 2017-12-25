9719588863948716963b994e58572b83eb748eb5
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-25 18:07:28 +0100

    Added CategoryController class

---
+++ src/mvc/controller/CategoryController.java
@@ -0,0 +1,8 @@
+package mvc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class CategoryController {
+	private static final Logger log = LoggerFactory.getLogger(CategoryController.class);
+}
