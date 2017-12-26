2a8e251c2eebb4fce5c5aab96e161f59bbbfdb8b
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-26 13:27:15 +0100

    Get urls from selection

--- src/mvc/controller/UrlController.java
+++ src/mvc/controller/UrlController.java
@@ -1,39 +1,67 @@
 package mvc.controller;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import mvc.dao.DAOFactory;
+import mvc.dao.model.ICategoryDAO;
+import mvc.dao.model.IMainCategoryDAO;
 import mvc.dao.model.IUrlDAO;
 import mvc.model.Category;
 import mvc.model.MainCategory;
+import mvc.model.Url;
 import mvc.view.observer.category.CategorySelectedListener;
 
 public final class UrlController implements CategorySelectedListener {
 	private static final Logger log = LoggerFactory.getLogger(UrlController.class);
 	
 	private IUrlDAO urlDao = null;
+	private ICategoryDAO catDao = null;
 	
 	public UrlController() {
 		log.info("Initialize url controller");
 		
 		//TODO add posibility to get dao factory without parameter (load selected database index from file)
 		urlDao = DAOFactory.get(DAOFactory.SQLITE).getUrl();
+		catDao = DAOFactory.get(DAOFactory.SQLITE).getCategory();
 	}
 
 	@Override
 	public void onMainCategorySelect(List<MainCategory> categories) {
 		log.debug("Main categories selected");
 		
-		for(MainCategory cat : categories) log.warn(cat.toString());
+		List<Url> urls = new LinkedList<>();
+		
+		for(MainCategory cat : categories) {
+			log.debug("Get all subcategories from {} category", cat.toString());
+			List<Category> subcategories = catDao.getWithMainCategory(cat);
+			log.debug("Found {} subcategories", subcategories.size());
+			
+			for(Category subcat : subcategories) {
+				log.debug("Get all urls from {} subcategory", subcat.toString());
+				
+				urls.addAll(urlDao.getAllWithCategory(subcat));
+			}
+		}
+		
+		log.debug("Found {} urls", urls.size());
 	}
 
 	@Override
 	public void onCategorySelect(List<Category> categories) {
 		log.debug("Main categories selected");
 		
-		for(Category cat : categories) log.warn(cat.toString());
+		List<Url> urls = new LinkedList<>();
+		
+		for(Category subcat : categories) {
+			log.debug("Get all urls from {} subcategory", subcat.toString());
+			
+			urls.addAll(urlDao.getAllWithCategory(subcat));
+		}
+		
+		log.debug("Found {} urls", urls.size());
 	}
 }
