2d33cf65be37504cb1379a65cee30c4834a512c5
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-26 13:11:07 +0100

    Implement category selected interface to url controller

--- src/mvc/controller/UrlController.java
+++ src/mvc/controller/UrlController.java
@@ -1,12 +1,17 @@
 package mvc.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import mvc.dao.DAOFactory;
 import mvc.dao.model.IUrlDAO;
+import mvc.model.Category;
+import mvc.model.MainCategory;
+import mvc.view.observer.category.CategorySelectedListener;
 
-public final class UrlController {
+public final class UrlController implements CategorySelectedListener {
 	private static final Logger log = LoggerFactory.getLogger(UrlController.class);
 	
 	private IUrlDAO urlDao = null;
@@ -16,5 +21,19 @@ public final class UrlController {
 		
 		//TODO add posibility to get dao factory without parameter (load selected database index from file)
 		urlDao = DAOFactory.get(DAOFactory.SQLITE).getUrl();
+	}
+
+	@Override
+	public void onMainCategorySelect(List<MainCategory> categories) {
+		log.debug("Main categories selected");
+		
+		for(MainCategory cat : categories) log.warn(cat.toString());
+	}
+
+	@Override
+	public void onCategorySelect(List<Category> categories) {
+		log.debug("Main categories selected");
+		
+		for(Category cat : categories) log.warn(cat.toString());
 	}
 }
