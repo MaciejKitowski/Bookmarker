18fd92031d8090c005b029b1ecb45b08f51dadba
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-27 11:59:40 +0100

    Remove old observer

--- src/mvc/controller/observer/url/UrlChangedListener.java
+++
@@ -1,9 +0,0 @@
-package mvc.controller.observer.url;
-
-import java.util.List;
-import mvc.model.Url;
-
-@Deprecated
-public interface UrlChangedListener {
-	public void onUrlChanged(List<Url> urls);
-}
