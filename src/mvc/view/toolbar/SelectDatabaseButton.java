3c72572a00ba174a88556495446b196fd81dd846
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-28 14:51:40 +0100

    Add actionListener interface

--- src/mvc/view/toolbar/SelectDatabaseButton.java
+++ src/mvc/view/toolbar/SelectDatabaseButton.java
@@ -1,6 +1,8 @@
 package mvc.view.toolbar;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
@@ -9,7 +11,7 @@ import javax.swing.JButton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class SelectDatabaseButton extends JButton {
+public final class SelectDatabaseButton extends JButton implements ActionListener {
 	private static final long serialVersionUID = -3802417046411311406L;
 	private static final Logger log = LoggerFactory.getLogger(SelectDatabaseButton.class);
 	private static final String iconName = "toolbar_database.png";
@@ -17,19 +19,35 @@ public final class SelectDatabaseButton extends JButton {
 	public SelectDatabaseButton(Dimension size) {
 		log.info("Create select database button");
 		
+		initializeSize(size);
+		initializeIcon();
+	}
+	
+	private void initializeSize(Dimension size) {
+		log.debug("Set size: {}", size.toString());
+		
 		setSize(size);
 		setPreferredSize(size);
 		setMinimumSize(size);
 		setMaximumSize(size);
+	}
+	
+	private void initializeIcon() {
+		log.debug("Set icon");
 		
 		try {
-			log.debug("Set icon");
-			
 			URL icon = getClass().getResource(String.format("/icons/%s", iconName));
 			setIcon(new ImageIcon(icon));
 		}
 		catch(Exception ex) {
 			log.error("Failed to set icon", ex);
 		}
+		
+		addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		log.debug("Pressed button");
 	}
 }
