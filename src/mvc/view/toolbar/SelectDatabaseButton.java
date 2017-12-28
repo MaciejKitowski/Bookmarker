5c72e31a4334773dfb67a60e599db329b3d20c6e
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-28 15:08:33 +0100

    Add radio buttons

--- src/mvc/view/toolbar/SelectDatabaseButton.java
+++ src/mvc/view/toolbar/SelectDatabaseButton.java
@@ -5,13 +5,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
 
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.JRadioButton;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import mvc.dao.DAOFactory;
 
 public final class SelectDatabaseButton extends JButton implements ActionListener {
 	private static final long serialVersionUID = -3802417046411311406L;
@@ -54,11 +58,30 @@ public final class SelectDatabaseButton extends JButton implements ActionListene
 	private void initializePopup() {
 		log.debug("Initialize popup");
 		
-		popup = new JPopupMenu();
+		JRadioButton sqlite = new JRadioButton("SqLite", true);
+		sqlite.setActionCommand(String.valueOf(DAOFactory.SQLITE));
 		
-		popup.add(new JMenuItem("SqLite"));
+		JRadioButton mysql = new JRadioButton("My-SQL");
+		sqlite.setActionCommand(String.valueOf(DAOFactory.MYSQL));
+		
+		JRadioButton postg = new JRadioButton("PostgreSQL");
+		sqlite.setActionCommand(String.valueOf(DAOFactory.POSTGRES));
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(sqlite);
+		group.add(mysql);
+		group.add(postg);
+
+		popup = new JPopupMenu();
+		popup.add(sqlite);
+		popup.add(mysql);
+		popup.add(postg);
+		
+		
+		
+		/*popup.add(new JMenuItem("SqLite"));
 		popup.add(new JMenuItem("My-SQL"));
-		popup.add(new JMenuItem("PostgreSQL"));
+		popup.add(new JMenuItem("PostgreSQL"));*/
 	}
 
 	@Override
