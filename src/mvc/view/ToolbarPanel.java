f08055d2ba000034fa477e72252e0dacf1b88d8e
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-28 12:41:29 +0100

    Generate button method

--- src/mvc/view/ToolbarPanel.java
+++ src/mvc/view/ToolbarPanel.java
@@ -2,10 +2,18 @@ package mvc.view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +22,8 @@ public final class ToolbarPanel extends JPanel {
 	private static final Logger log = LoggerFactory.getLogger(ToolbarPanel.class);
 	private static final int spaceBetweenComponent = 10;
 	
+	private final Dimension buttonSize;
+	
 	public ToolbarPanel(int width, int height) {
 		log.info("Initialize toolbar");
 		
@@ -21,10 +31,29 @@ public final class ToolbarPanel extends JPanel {
 		setPreferredSize(new Dimension(width, height));
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		
-		add(new JButton("1"));
-		add(Box.createHorizontalStrut(spaceBetweenComponent));
-		add(new JButton("2"));
-		add(Box.createHorizontalStrut(spaceBetweenComponent));
-		add(new JButton("3"));
+		buttonSize = new Dimension(height - 10, height - 10);
+		
+		generateButton("toolbar_addnew.png");
+	}
+	
+	private JButton generateButton(String iconName) {
+		try {
+			URL icon = getClass().getResource(String.format("/icons/%s", iconName));
+			JButton button = new JButton(new ImageIcon(icon));
+			
+			button.setPreferredSize(buttonSize);
+			button.setMaximumSize(buttonSize);
+			button.setMinimumSize(buttonSize);
+			button.setSize(buttonSize);
+			
+			add(Box.createHorizontalStrut(spaceBetweenComponent));
+			add(button);
+			
+			return button;
+		}
+		catch(Exception ex) {
+			log.error("Failed to generate button", ex);
+			return null;
+		}
 	}
 }
