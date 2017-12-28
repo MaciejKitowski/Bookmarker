dc04d63e34bb9ac6b5ee1219a7e2417a0b36389a
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-28 10:59:44 +0100

    Add class

---
+++ src/mvc/view/ToolbarPanel.java
@@ -0,0 +1,11 @@
+package mvc.view;
+
+import javax.swing.JPanel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ToolbarPanel extends JPanel {
+	private static final long serialVersionUID = 1071937589387532800L;
+	private static final Logger log = LoggerFactory.getLogger(ToolbarPanel.class);
+}
