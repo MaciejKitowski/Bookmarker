384b5a38d3096cfbd185653da5819214a12d58cd
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-28 14:11:29 +0100

    Minor changes in toolbar buttons position

--- src/mvc/view/ToolbarPanel.java
+++ src/mvc/view/ToolbarPanel.java
@@ -2,46 +2,41 @@ package mvc.view;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Image;
 import java.net.URL;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
+import javax.swing.JSeparator;
+import javax.swing.JToolBar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class ToolbarPanel extends JPanel {
+public final class ToolbarPanel extends JToolBar {
 	private static final long serialVersionUID = 1071937589387532800L;
 	private static final Logger log = LoggerFactory.getLogger(ToolbarPanel.class);
-	private static final int spaceBetweenComponent = 10;
 	
 	private final Dimension buttonSize;
 	
 	public ToolbarPanel(int width, int height) {
 		log.info("Initialize toolbar");
 		
-		setBackground(Color.CYAN);
-		setPreferredSize(new Dimension(width, height));
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		
 		buttonSize = new Dimension(height - 5, height - 5);
 		
-		generateButton("toolbar_addnew.png");
-		generateButton("toolbar_delete.png");
-		generateButton("toolbar_edit.png");
-		generateButton("toolbar_database.png");
+		setBackground(Color.CYAN);
+		setPreferredSize(new Dimension(width, height));
+		setFloatable(false);
+		setBorder(BorderFactory.createEmptyBorder());
+
+		generateButton("toolbar_addnew.png", 10);
+		generateButton("toolbar_edit.png", 5);
+		generateButton("toolbar_delete.png", 5);
+		generateButton("toolbar_database.png", 30);
 		//generateButton("toolbar_settings.png");
-		
 	}
 	
-	private JButton generateButton(String iconName) {
+	private JButton generateButton(String iconName, int spaceBefore) {
 		try {
 			URL icon = getClass().getResource(String.format("/icons/%s", iconName));
 			JButton button = new JButton(new ImageIcon(icon));
@@ -51,7 +46,7 @@ public final class ToolbarPanel extends JPanel {
 			button.setMinimumSize(buttonSize);
 			button.setSize(buttonSize);
 			
-			add(Box.createHorizontalStrut(spaceBetweenComponent));
+			add(Box.createHorizontalStrut(spaceBefore));
 			add(button);
 			
 			return button;
