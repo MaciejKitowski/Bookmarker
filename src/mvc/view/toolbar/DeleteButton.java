7d21b90afa3486c548554d98a2ae49127b8f5c69
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-29 11:57:09 +0100

    Initialize button

--- src/mvc/view/toolbar/DeleteButton.java
+++ src/mvc/view/toolbar/DeleteButton.java
@@ -3,7 +3,9 @@ package mvc.view.toolbar;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 import org.slf4j.Logger;
@@ -11,13 +13,37 @@ import org.slf4j.LoggerFactory;
 
 public final class DeleteButton extends JButton implements ActionListener {
 	private static final long serialVersionUID = 5739651433521986611L;
-	private static final Logger log = LoggerFactory.getLogger(SelectDatabaseButton.class);
+	private static final Logger log = LoggerFactory.getLogger(DeleteButton.class);
 	private static final String iconName = "toolbar_delete.png";
 	
 	public DeleteButton(Dimension size) {
 		log.info("Create delete button");
+		
+		initializeSize(size);
+		initializeIcon();
 				
 		addActionListener(this);
+	}
+	
+	private void initializeSize(Dimension size) {
+		log.debug("Set size: {}", size.toString());
+		
+		setSize(size);
+		setPreferredSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+	}
+	
+	private void initializeIcon() {
+		log.debug("Set icon");
+		
+		try {
+			URL icon = getClass().getResource(String.format("/icons/%s", iconName));
+			setIcon(new ImageIcon(icon));
+		}
+		catch(Exception ex) {
+			log.error("Failed to set icon", ex);
+		}
 	}
 
 	@Override
