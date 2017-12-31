830e5aa8f57aa44b8f8dde8251363bf64eb0ae3f
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-31 21:57:01 +0100

    Add fields to subcategory panel

--- src/mvc/view/toolbar/button/SubcategoryPanel.java
+++ src/mvc/view/toolbar/button/SubcategoryPanel.java
@@ -1,12 +1,18 @@
 package mvc.view.toolbar.button;
 
 import java.awt.GridLayout;
+import java.util.List;
 
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import mvc.dao.DAOFactory;
+import mvc.model.Category;
 import mvc.model.Subcategory;
 
 final class SubcategoryPanel extends JPanel {
@@ -15,6 +21,12 @@ final class SubcategoryPanel extends JPanel {
 	
 	private boolean displayID = false;
 	private Subcategory subcategory = null;
+	private JLabel idLabel = new JLabel("ID");
+	private JLabel nameLabel = new JLabel("Name");
+	private JLabel selectCategoryLabel = new JLabel("Select category");
+	private JTextField idField = new JTextField();
+	private JTextField nameField = new JTextField();
+	private JComboBox<Category> selectCategoryField = null;
 
 	public SubcategoryPanel() {
 		log.info("Initialize subcategory panel");
@@ -23,9 +35,45 @@ final class SubcategoryPanel extends JPanel {
 		initializePanel();
 	}
 	
+	public SubcategoryPanel(Subcategory subcategory) {
+		log.info("Initialize subcategory panel with values");
+		
+		displayID = true;
+		this.subcategory = subcategory;
+		initializePanel();
+	}
+	
 	private void initializePanel() {
 		log.debug("Initialize panel");
 		
 		setLayout(new GridLayout(0, 1));
+		
+		if(subcategory != null) {
+			idField.setText(String.valueOf(subcategory.getID()));
+			nameField.setText(subcategory.getName());
+			
+			List<Category> categories = DAOFactory.get().getMainCategory().getAll();
+			Category current = null;
+			for(Category cat : categories) {
+				if(subcategory.getParent().getID() == cat.getID()) {
+					current = cat;
+					break;
+				}
+			}
+			
+			selectCategoryField = new JComboBox<>(categories.toArray(new Category[categories.size()]));
+			selectCategoryField.setSelectedItem(current);
+		}
+		
+		if(displayID) {
+			idField.setEnabled(false);
+			add(idLabel);
+			add(idField);
+		}
+		
+		add(selectCategoryLabel);
+		add(selectCategoryField);
+		add(nameLabel);
+		add(nameField);
 	}
 }
