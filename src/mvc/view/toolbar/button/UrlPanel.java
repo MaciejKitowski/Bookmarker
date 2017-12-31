b8c03c460b6cec63b69b914cfed17e6bc5a2c404
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-31 22:06:00 +0100

    Add url panel class

---
+++ src/mvc/view/toolbar/button/UrlPanel.java
@@ -0,0 +1,21 @@
+package mvc.view.toolbar.button;
+
+import javax.swing.JPanel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import mvc.model.Url;
+
+final class UrlPanel extends JPanel {
+	private static final long serialVersionUID = -7033793384872165623L;
+	private static final Logger log = LoggerFactory.getLogger(UrlPanel.class);
+	
+	public UrlPanel() {
+		
+	}
+	
+	public UrlPanel(Url url) {
+		
+	}
+}
