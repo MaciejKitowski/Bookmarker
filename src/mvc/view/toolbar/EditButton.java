73d38b730258bd18e4dfa2eb7bb824867c05097b
Maciej Kitowski <maciejkitowski@hotmail.com>
2017-12-31 19:38:36 +0100

    Add class

---
+++ src/mvc/view/toolbar/EditButton.java
@@ -0,0 +1,11 @@
+package mvc.view.toolbar;
+
+import javax.swing.JButton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class EditButton extends JButton {
+	private static final long serialVersionUID = 2452055567420326318L;
+	private static final Logger log = LoggerFactory.getLogger(EditButton.class);
+}
