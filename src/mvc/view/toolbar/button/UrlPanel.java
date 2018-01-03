699d2090a3d1a796c77cd21380aeef2d53e9a31a
Maciej Kitowski <maciejkitowski@hotmail.com>
2018-01-03 13:03:16 +0100

    Better url add/edit form

--- src/mvc/view/toolbar/button/UrlPanel.java
+++ src/mvc/view/toolbar/button/UrlPanel.java
@@ -1,11 +1,14 @@
 package mvc.view.toolbar.button;
 
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.List;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -23,21 +26,23 @@ final class UrlPanel extends JPanel {
 	
 	private boolean displayID = false;
 	private Url url = null;
+	private GridBagConstraints constraints = new GridBagConstraints();
 	private JLabel idLabel = new JLabel("ID");
 	private JLabel titleLabel = new JLabel("Title");
 	private JLabel urlLabel = new JLabel("Url");
 	private JLabel descriptionLabel = new JLabel("Description");
-	private JLabel selectSubcategoryLabel = new JLabel("Select subcategory");
+	private JLabel selectSubcategoryLabel = new JLabel("Subcategory");
 	private JTextField idField = new JTextField();
 	private JTextField titleField = new JTextField();
 	private JTextField urlField = new JTextField();
-	private JTextArea descriptionField = new JTextArea();
+	private JTextArea descriptionField = new JTextArea(4, 20);
 	private JComboBox<Subcategory> selectSubcategoryField = null;
 	
 	public UrlPanel() {
 		log.info("Initialize url panel");
 		
 		displayID = false;
+		idField.setEnabled(false);
 		initializePanel();
 	}
 	
@@ -46,13 +51,16 @@ final class UrlPanel extends JPanel {
 		
 		displayID = true;
 		this.url = url;
+		idField.setEnabled(false);
 		initializePanel();
 	}
 	
 	private void initializePanel() {
 		log.debug("Initialize panel");
 		
-		setLayout(new GridLayout(0, 1));
+		setLayout(new GridBagLayout());
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.insets = new Insets(0, 0, 5, 0);
 		
 		List<Subcategory> subcategories = DAOFactory.get().getSubcategory().getAll();
 		selectSubcategoryField = new JComboBox<>(subcategories.toArray(new Subcategory[subcategories.size()]));
@@ -76,19 +84,58 @@ final class UrlPanel extends JPanel {
 		}
 
 		if(displayID) {
-			idField.setEnabled(false);
-			add(idLabel);
-			add(idField);
+			constraints.gridx = 0;
+			constraints.gridy = 0;
+			constraints.weightx = 0.05;
+			add(idLabel, constraints);
+			
+			constraints.gridx = 1;
+			constraints.gridy = 0;
+			constraints.weightx = 0.95;
+			add(idField, constraints);
 		}
 		
-		add(titleLabel);
-		add(titleField);
-		add(urlLabel);
-		add(urlField);
-		add(descriptionLabel);
-		add(descriptionField);
-		add(selectSubcategoryLabel);
-		add(selectSubcategoryField);
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		constraints.weightx = 0.05;
+		add(titleLabel, constraints);
+		
+		constraints.gridx = 1;
+		constraints.gridy = 1;
+		constraints.weightx = 0.95;
+		add(titleField, constraints);
+		
+		
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		constraints.weightx = 0.05;
+		add(urlLabel, constraints);
+		
+		constraints.gridx = 1;
+		constraints.gridy = 2;
+		constraints.weightx = 0.95;
+		add(urlField, constraints);
+		
+		
+		constraints.gridx = 0;
+		constraints.gridy = 3;
+		constraints.weightx = 0.05;
+		add(descriptionLabel, constraints);
+		
+		constraints.gridx = 1;
+		constraints.gridy = 3;
+		constraints.weightx = 0.95;
+		add(new JScrollPane(descriptionField), constraints);
+		
+		constraints.gridx = 0;
+		constraints.gridy = 4;
+		constraints.weightx = 0.05;
+		add(selectSubcategoryLabel, constraints);
+		
+		constraints.gridx = 1;
+		constraints.gridy = 4;
+		constraints.weightx = 0.95;
+		add(selectSubcategoryField, constraints);
 	}
 	
 	public boolean isDescriptionEmpty() {
